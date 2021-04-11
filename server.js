4faf077343c8934ce7686cae0ba70984698cb3cf
gshaver <geneshaver@gmail.com>
2021-04-11 00:36:54 -0500

    restructured files and folders

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const routes = require("./routes");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/recipes_hybrid_DB",
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+app.use(routes);
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function () {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
