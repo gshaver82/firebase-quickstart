792cce25f42d7a2143eac0af34f293ad9d202c46
gshaver <geneshaver@gmail.com>
2021-04-11 04:02:56 -0500

    added routes

---
+++ controllers/exampleController.js
@@ -0,0 +1,10 @@
+// const db = require("../models");
+// module.exports = {
+//     findAll: function (req, res) {
+//         db.RecipesModel
+//             .find()
+//             .then(dbModelDataResult => res.json(dbModelDataResult))
+//             .catch(err => res.status(422).json(err));
+//     },
+
+// };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-
+const exampleRoutes = require("/example");
 // Recipe routes
-router.use("/xxx", xxx);
+router.use("/example", exampleRoutes);
 
 module.exports = router;
